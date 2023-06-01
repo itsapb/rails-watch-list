9462d2c434fe661ad24f44c763f4e423fd686a16
Alice Palmer Brown <palmerbrownalice@gmail.com>
2023-06-01 15:55:04 +0100

    with bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get lists
+
+  get 'lists', to: 'lists#index'
+  get 'lists/new', to: 'lists#new'
+  post 'lists', to: 'lists#create'
+  get 'lists/:id', to: 'lists#show', as: :list
+
+  # get bookmarks
+
+  get 'lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: :new_bookmark
+  post 'lists/:list_id/bookmarks', to: 'bookmarks#create', as: :bookmarks
+  delete 'lists/:list_id/bookmarks/:id', to: 'bookmarks#destroy', as: :delete_bookmark
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-
 require 'json'
 require 'uri'
 require 'net/http'
@@ -27,7 +26,7 @@ puts response.read_body.class
 JSON.parse(response.read_body)['results'].each do |movie|
   Movie.create(title: movie['original_title'],
                overview: movie['overview'],
-               poster_url: movie['poster_path'],
+               poster_url: "https://image.tmdb.org/t/p/w500/#{poster_url}",
                rating: movie['vote_average'])
 end
 
