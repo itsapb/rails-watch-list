0322b86892da32b8c1b00add7f0581a2a34c6684
Alice Palmer Brown <palmerbrownalice@gmail.com>
2023-06-02 15:22:26 +0100

    dependendent destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ puts response.read_body.class
 JSON.parse(response.read_body)['results'].each do |movie|
   Movie.create(title: movie['original_title'],
                overview: movie['overview'],
-               poster_url: "https://image.tmdb.org/t/p/w500/#{poster_url}",
+               poster_url: movie['poster_url'],
                rating: movie['vote_average'])
 end
 
