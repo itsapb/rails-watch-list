073f24fadd6ddfa9fb397e56708d0cd7c26cc0c7
Alice Palmer Brown <palmerbrownalice@gmail.com>
2023-06-02 15:09:03 +0100

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
-
+  root to: "lists#index"
   # get lists
   resources :lists, only: [:destroy, :index, :new, :create, :show]
 
